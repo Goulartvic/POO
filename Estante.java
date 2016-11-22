3fc3c705adc4826682a14a8ba2de4b835c192d72
Victor Goulart <goulart.vic@gmail.com>
2016-11-22 11:10:20 -0200

    Add files via upload

---
+++ Estante.java
@@ -0,0 +1,16 @@
+
+public class Estante {
+	
+	private Item[] i;
+	private Usuario[] u;
+	
+	public boolean alocarItem(){
+		return true;
+	}
+	
+	public boolean devolverItem(){
+		return true;
+	}
+	
+
+}
---
+++ Livro.java
@@ -0,0 +1,20 @@
+public class Livro extends Item {
+	
+	private String autor;
+	
+	public Livro(String autor, String codigo, boolean alocado, String titulo){
+		super();
+		this.setAutor(autor);
+		super.setCodigo(codigo);
+		super.setAlocado(alocado);
+		super.setTitulo(titulo);
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+}
